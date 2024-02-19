b941b374b9830bc48dd4854c921943d723276220
Yuniski <yuniski24@gmail.com>
2024-02-19 18:35:38 -0300

    adding author popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
-// Import our custom CSS
 import "../scss/styles.scss";
-
-// Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+import Swal from "sweetalert2";
+
+const author = document.getElementById("author");
+
+author.addEventListener("click", () => {
+  const darkBlue = "#1D2D44";
+  const light = "#F0EBD8";
+
+  Swal.fire({
+    title: "Thanks for watching!",
+    text: "In this section you can find useful links to check out my work and contact me ❤️",
+    // color: darkBlue,
+    color: darkBlue,
+    background: light,
+    icon: "info",
+    confirmButtonText: "Ok!",
+  });
+});
